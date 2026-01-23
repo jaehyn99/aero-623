6f623f66b54e0887ed77e77a09e885a1771525d1
Jaehyeon Song <jaehyn@umich.edu>
2026-01-23 14:02:47 -0500

    Add a headers and sources for projection-based local refinement of mesh

---
+++ src/mesh/BladeGeometry.cpp
@@ -0,0 +1,13 @@
+#include "mesh/BladeGeometry.h"
+
+#include <fstream>
+#include <limits>
+#include <cmath>
+
+void BladeGeometry::load(const std::string& upperfile,
+                         const std::string& lowerfile)
+{
+    nodes_.clear();
+    
+    
+}
