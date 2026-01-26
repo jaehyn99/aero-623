f6d28f0abb166668a792490705b1efc557dc49e5
mmarusich29 <mmarus@Maddie-Dell.localdomain>
2026-01-26 15:13:55 -0500

    Initial class structure for loading .gri file

--- projects/Project-1/project1Task3.cpp
+++ projects/Project-1/project1Task3.cpp
@@ -4,9 +4,49 @@
 #include <sstream>
 #include <iostream>
 
-GRIData readFile() {
+GRIData readGriFile(const std::string& filename) {
 
-    //add here
+    std::ifstream file(filename);
+    if (!file) throw std::runtime_error("Could not open file " + filename);
+
+    Map gridMap;
+
+    file >> gridMap.nNode >> gridMap.nElemTot >> gridMap.Dim;
+
+    // Saves node coordinates
+    std::vector<double> point;
+    for (int i=0;i<gridMap.nNode;i++){
+        double x, y, z = 0.0;
+        file >> x >> y;
+        if (gridMap.Dim == 3) {
+            file >> z;
+        }
+        point = {x, y, z};
+        gridMap.nodeXYZ.push_back(point);
+    }
+
+    // Saving boundary data
+    file >> gridMap.nBGroup;
+    int nBFace, nf;
+    std::string title;
+    std::vector<int> nb;
+    int nbTemp;
+    BoundaryGroup boundary;
+    for (int i=0;i<gridMap.nBGroup;i++) {
+        file >> nBFace >> nf >> title;
+        // std::string line;
+        // if (std::getline(file, line)) {  // Read one line from file
+        //     std::istringstream iss(line); // Use stringstream to parse the line
+        //     iss >> nBFace >> nf >> title; // Extract the first two ints and the string
+        // }
+        for (int j=0;j<nBFace;j++) {
+            file >> nbTemp;
+            nb.push_back(nbTemp);
+        }
+        boundary
+
+
+    }
 }
 
 // ReadGRI::readFile(const std::string& filename)
