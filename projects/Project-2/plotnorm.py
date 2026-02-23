f9d2a9d1c93c4b737b84b096ca67a3b41980f9cf
U-DESKTOP-2LOH3R2\dbloc <dbloc@umich.edu>
2026-02-22 23:39:58 -0500

    unsteady norm plots

--- projects/Project-2/plotnorm.py
+++ projects/Project-2/plotnorm.py
@@ -9,13 +9,12 @@ def main():
     argc = len(sys.argv)
     plt.figure()
     legend = [] if argc <= 2 else sys.argv[1:]
-    print(legend)
     for i in range(1, argc):
         norm = np.loadtxt(sys.argv[i])
         iter = np.arange(norm.size)
         plt.semilogy(iter, norm)
     if argc > 2:
-        plt.legend(["RK2", "RK3"])
+        plt.legend(["M1", "M2", "M3"])
     plt.xlabel("Iteration number")
     plt.ylabel("L1 residual norm")
     plt.show()
