69c44d9c2429c3db74e237003fe02de58e783641
wcurtmaxon <wmaxon@umich.edu>
2026-03-10 16:47:09 -0400

    Working quadrature, reference element, and shape functions

---
+++ include/quadrature/femQuadrature.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <Eigen/Dense>
+
+class femQuadrature {
+	public:
+		femQuadrature(int q) : q_(q) {}
+		virtual Eigen::MatrixXd getQuadXi(int q_) const;
+		virtual Eigen::VectorXd getQuadW(int q_) const;
+		
+	protected:
+		int q_;
+};
---
+++ tests/shapeFunctions/testShapeFunctions.cpp
@@ -0,0 +1,17 @@
+#include "shapeFunctions/shapeFunctions.hpp"
+#include <iostream>
+#include <Eigen/Dense>
+
+int main() {
+	int p = 3;
+	shapeFunctions shape(p);
+	Eigen::MatrixXd phiCoeffs = shape.getShapeFuncCoeffs(p);
+	Eigen::MatrixXd phiXiCoeffs = shape.getShapeFuncXiCoeffs(p);
+	Eigen::MatrixXd phiEtaCoeffs = shape.getShapeFuncEtaCoeffs(p);
+	Eigen::MatrixXd xiL = shape.getRefLagrangePoints(p);
+	std::cout << phiCoeffs << std::endl;
+	std::cout << phiXiCoeffs << std::endl;
+	std::cout << phiEtaCoeffs << std::endl;
+	std::cout << xiL << std::endl;
+	return 0;
+}
