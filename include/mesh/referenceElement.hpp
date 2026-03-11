c92c1ae87f9c7ac2f4fb48d1965c74d560ba5b5e
wcurtmaxon <wmaxon@umich.edu>
2026-03-11 15:27:22 -0400

    added comments to code

--- include/mesh/referenceElement.hpp
+++ include/mesh/referenceElement.hpp
@@ -19,21 +19,23 @@ class referenceElement {
 			shapeFunctions shape(p_);
 			femQuadrature quad(q_);
 			xiL = shape.getRefLagrangePoints(p_);
-			Eigen::MatrixXd phiCoeffs = shape.getShapeFuncCoeffs(p_);
+			Eigen::MatrixXd phiCoeffs = shape.getShapeFuncCoeffs(p_); // Gets shape function and derivative coefficients
 			Eigen::MatrixXd phiXiCoeffs = shape.getShapeFuncXiCoeffs(p_);
 			Eigen::MatrixXd phiEtaCoeffs = shape.getShapeFuncEtaCoeffs(p_);
 
-			xiQ = quad.getQuadXi(q_);
+			xiQ = quad.getQuadXi(q_); // Gets quadrature points in reference space and weights
 			wQ = quad.getQuadW(q_);
 			nQ = wQ.size();
-			nL = (p_ + 1)*(p_ + 2)/2;
-			Eigen::VectorXd mon(nL);
+			nL = (p_ + 1)*(p_ + 2)/2; // Computes number of Lagrange points per triangle
+			Eigen::VectorXd mon(nL); 
 			phi.resize(nQ, nL);
 			phiXi.resize(nQ, nL);
 			phiEta.resize(nQ, nL);
 			MRef.resize(nL, nL);
-
+			
 			phi.setZero(); phiXi.setZero(); phiEta.setZero(); MRef.setZero();
+
+			// Computes monomial coefficients
 			for (int ii = 0; ii < nQ; ii++) {
 				for (int jj = 0; jj < p_ + 1; jj++) {
 					for (int kk = 0; kk < jj + 1; kk++) {
@@ -41,6 +43,7 @@ class referenceElement {
 					}
 				}
 
+				// computes shape functions and derivatives at quadrature points
 				for (int jj = 0; jj < nL; jj++) {
 					for (int kk = 0; kk < nL; kk++) {
 						phi(ii, jj) = phi(ii, jj) + phiCoeffs(jj, kk)*mon(kk);
@@ -52,6 +55,7 @@ class referenceElement {
 				}
 			}
 
+			// Computes reference mass matrix using quadrature
 			for (int ii = 0; ii < nL; ii++) {
 				for (int jj = 0; jj < nL; jj++) {
 					for (int kk = 0; kk < nQ; kk++) {
