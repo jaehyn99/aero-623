063e06683cb553b201bd363ba37a1bf6c02b941d
niranjann-14 <nn18@illinois.edu>
2026-03-13 15:26:05 -0400

    will add cuved elements code to this

---
+++ include/mesh/CurvedMesh.h
@@ -0,0 +1,14 @@
+#ifndef CURVED_MESH_H
+#define CURVED_MESH_H
+
+#include "Eigen/Dense"
+#include <string>
+#include <vector>
+#include "TriangularMesh.h"
+
+class CurvedMesh{
+    public:
+    void curved_mesh(TriangularMesh& mesh, const Eigen::MatrixXi& B2E, int order);
+};
+
+#endif
---
+++ src/mesh/CurvedMesh.cpp
@@ -0,0 +1,33 @@
+#include "CurvedMesh.h"
+#include "TriangularMesh.h"
+
+#include "Eigen/Dense"
+
+void CurvedMesh::curved_mesh(TriangularMesh& mesh, const Eigen::MatrixXi& B2E, int order)
+{
+    // loop over B2E and curve the edges and subsequently the nodes at the p=3 order
+    for (int f_b = 0; f_b < B2E.rows(); f_b++)
+    {
+        // get basis function values at quadrature points for each 1D lagrange polynomial
+
+
+        // get the coordinates of the nodes from cubic spline on the edge and curve them using the basis functions
+
+        // 
+
+    }
+}
+
+void CurvedMesh::curved_mesh_alt(TriangularMesh& mesh, const Eigen::MatrixXi& B2E, int order)
+{
+    // loop over B2E and curve the edges at order p=3
+    for (int f_b = 0; f_b < B2E.rows(); f_b++)
+    {
+        // get Jacobian of element mapping from reference element to physical element
+        // get dx_vec/d(zeta) and dx_vec/d(eta) at the edge quadtrature points for base of reference traingle where d(zeta)/d(sig) = 1 and d(eta)/d(sig) = 0)
+
+        // normalize this to get the unit tangent vector at the edge quadrature points
+
+        // normal vector at the edge quadrature points is then given by rotating the tangent vector by 90 degrees
+    }
+}
